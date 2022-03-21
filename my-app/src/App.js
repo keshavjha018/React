817d1efaff0685953246594808dff81955e26e8c
Keshav <keshavjha018@gmail.com>
2022-03-21 23:49:18 +0530

    updated

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,31 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from "react";
 
-// function based component
-let myname = "Keshav"
 function App() {
   return (
     // JSX 
     <>
-    <h1 className="name-heading"> My name is {myname} </h1>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with Keshav
-        </a>
-      </header>
-    </div>
+      <Greet />
+      <Greet />
+      <h1>Keshav Jha</h1>
+      
     </>
   );
-}
+};
 
+//component (fun- based)
+const Greet = () => {
+  return <h1>Hello World</h1>;
+};
+
+// export App fun to index.js
 export default App;
